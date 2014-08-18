b8679229828e0ddcd732f35b92d6761778fb34d0
JordanByrd <jordan.byrd@carestream.com>
2014-08-18 10:59:59 -0400

    init commit

---
+++ split.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var fs = require('fs');
+var paster = require("copy-paste");
+
+fs.readFile('C:\\home\\Bryce\\src\\Csh.Xrs.Web.Console\\App\\routes\\worklist\\worklist.html', 'utf-8', function (err, data) {
+	if (err) {
+		console.log(err);
+	}
+	var spilt = data.split('\n');
+	var finish = [];
+	for (var i = 0; i < spilt.length; i++) {
+		var stringent = spilt[i];
+		/*		console.log(stringent.length);
+		 */
+		if (stringent.length !== 1 && stringent.length !== 0) {
+			stringent = stringent.trim();
+			stringent = stringent.replace(/(\r\n|\n|\r)/gm, "");
+			stringent = stringent.replace(/'/g, "\\'");
+			stringent = '\'' + stringent + "\'";
+			if (i !== spilt.length - 1) {
+				stringent = stringent + " + ";
+			}
+			finish.push(stringent);
+		}
+	}
+	finish[finish.length - 1] = finish[finish.length - 1].replace(/\+/, ';');
+	finish = finish.join('\n');
+	paster.copy(finish, function () {
+		console.log('check your clipboard ');
+	});
+});
